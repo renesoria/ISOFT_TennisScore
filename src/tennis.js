aeed53f2b76d51fb1d3a33c2581385b26af2446d
Soria <rene.soria@ucb.edu.bo>
2026-09-01 10:26:13 -0400

    feat: muestra 15-Love cuadno el jugador 1 anota y jugador 2 en cero

--- src/tennis.js
+++ src/tennis.js
@@ -1,5 +1,15 @@
 class Tennis{
+
+    puntosJugador1 = 0;
+
+    jugador1_anota(){
+        this.puntosJugador1++;
+    }
+
     obtenerScore(){
+        if (this.puntosJugador1 == 1) {
+            return "15 - Love";
+        }
         return "Love - Love";
     }
 }
--- src/tennis.spec.js
+++ src/tennis.spec.js
@@ -6,5 +6,10 @@ describe("Tennis", () => {
     let tennis =new Tennis();
     expect(tennis.obtenerScore()).toEqual("Love - Love");
   });
+  //Cuando jugador 1 anota 1 vez --> "15-Love""
+    it("deberia mostrar 15-Love cuando el jugador 1 anota", () => {
+    let tennis =new Tennis();
+    tennis.jugador1_anota();
+    expect(tennis.obtenerScore()).toEqual("15 - Love");
 });
-
+});
